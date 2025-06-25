1dfce4d94359b8f728de6e7a1994f01192d8b19e
Christian Schladetsch <christian.schladetsch@gmail.com>
2025-06-26 00:11:03 +1000

    Delete g.sh

--- g.sh
+++
@@ -1,7 +0,0 @@
-  cd /home/xian/local/CppLiquid
-  git init
-  git add .
-  git commit -m "Initial commit: C++23 liquid simulation with OpenGL"
-  git remote add origin https://github.com/cschladetsch/CppOpenGLLiquid.git
-  git branch -M main
-  git push -u origin main
